702400ce1f6dd0c7b7231a4a2c423d84a49f59d6
julija9531 <julija1995@list.ru>
2023-05-30 16:30:42 +0300

    first commit

--- src/js/app/app.js
+++ src/js/app/app.js
@@ -7,27 +7,23 @@ export default class Validator {
     // Допустимы только латинские буквы, символы тире -, подчёркивания _ и цифры (0-9);
     let res = this.username.match(/[^a-zA-Z0-9-_]/);
     if (res !== null) {
-      console.log(res);
       return false;
     }
 
-    //Имя не должно содержать подряд более трёх цифр:
+    // Имя не должно содержать подряд более трёх цифр:
     res = this.username.match(/([0-9][0-9][0-9]+)/);
     if (res !== null) {
-      console.log(res);
       return false;
     }
-    //Имя не должно начинаться цифрами, символами подчёркивания или тире.
+    // Имя не должно начинаться цифрами, символами подчёркивания или тире.
     res = this.username.match(/^[0-9-_]/);
     if (res !== null) {
-      console.log(res);
       return false;
     }
 
-    //Имя не должно заканчиваться цифрами, символами подчёркивания или тире.
+    // Имя не должно заканчиваться цифрами, символами подчёркивания или тире.
     res = this.username.match(/[0-9-_]$/);
     if (res !== null) {
-      console.log(res);
       return false;
     }
 
